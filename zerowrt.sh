f902bc702a483aa471337cc115cbbede00eeb895
Jakues <jakueenak@gmail.com>
2022-09-26 08:01:43 +0700

    OpenClash: fix typo on sed

--- zerowrt.sh
+++ zerowrt.sh
@@ -256,7 +256,7 @@ Openclash () {
             # | awk 'FNR <= 1')
             export OC_Version=$(curl -sL https://github.com/vernesong/OpenClash/tags \
             | grep 'v0.45.' \
-            | sed -e 's/\"//g' -e 's/ //g' -e 's/rel=.*//g' -e 's#<ahref=##g' -e 's/>//g' -e 's#/vernesong/OpenClash/releases/tag/##g' e 's/v//g' \
+            | sed -e 's/\"//g' -e 's/ //g' -e 's/rel=.*//g' -e 's#<ahref=##g' -e 's/>//g' -e 's#/vernesong/OpenClash/releases/tag/##g' -e 's/v//g' \
             | awk 'FNR <= 1')
             export OC_Luci="https://github.com/vernesong/OpenClash/releases/download/v${OC_Version}/luci-app-openclash_${OC_Version}_all.ipk"
             wget -q -P packages/ ${OC_Luci} || error "Failed to download file:luci-app-openclash.ipk !"

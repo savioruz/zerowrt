2c9cfc5d58740392ec12db9da584c3ddf35b36e6
Jakues <jakueenak@gmail.com>
2022-11-08 15:11:28 +0700

    CI: imgbuilder.sh use header bash instead sh

--- config/imgbuilder/imgbuilder.sh
+++ config/imgbuilder/imgbuilder.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #================================================================================================
 #
 # This file is licensed under the terms of the GNU General Public
